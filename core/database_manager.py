a40a503c5237ddfabc9f7d4fa64d30b65bf8fe9d
matthew <guitarbass95@gmail.com>
2017-09-29 19:47:47 -0400

    started framing db manager

--- core/database_manager.py
+++ core/database_manager.py
@@ -1,1 +1,17 @@
-#database class 
+#database class
+
+import sqlite3
+
+
+class db_manager:
+
+    def __init__(self):
+        #check for existing dbs
+        #maybe have config file defining databases
+        #if not there, create from definitions
+        #if they exist, do nothing, other class
+        #functions will handle writing and reading
+
+    def insert(self, db_name, entry)
+        #i know nothing about db shit
+
