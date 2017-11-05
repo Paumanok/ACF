7862be08d1b6163c9e9f28ce5f5ae98c97d8c615
paumanok <guitarbass95@gmail.com>
2017-11-05 18:44:19 -0500

    lol putting ODT and XOJ into git. This wont end bad

--- core/db_types/feedlog.py
+++ core/db_types/feedlog.py
@@ -8,3 +8,7 @@ class feedlog:
         self.datetime = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
         #short fix for insert checks, datetime shouldnt be the same
         self.name = self.datetime
+
+
+    def __str__(self):
+        return self.__dict__
