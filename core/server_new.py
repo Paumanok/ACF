45afe94257c72a9a34adc3fa462d15c291c29696
Chris Ranc <clranc94@gmail.com>
2018-02-22 02:25:31 -0500

    added slave feeder verification end point

--- core/server_new.py
+++ core/server_new.py
@@ -9,6 +9,7 @@ from bson import objectid
 from bson.json_util import dumps
 from config import *
 import sys
+import hashlib
 
 app =  Flask("ACF")
 
@@ -20,7 +21,7 @@ resp501 = Response({"status": "501"}, status = 501, mimetype='application/json')
 def pet(pet_name):
     db = dbm().db
     pets = dbm().pets
-
+    resp = resp501
     if request.method == 'GET':
         if pets.find({"name":pet_name}).count() > 0:
             data = pets.find_one({"name":pet_name})
@@ -54,9 +55,53 @@ def pet(pet_name):
         resp = resp501
     else:
         #error 500
-        resp = resp501
+        #resp = resp501
         #probably goes against standards, talk about response formats with team
+        pass
     return resp
+
+
+@app.route('/sfeeder/config', methods = ['GET','POST'])
+def feeder_config():
+    feeders = dbm().feeders
+    resp = resp501
+    if request.method == 'GET':
+        if request.headers['Content-Type'] == 'application/json':
+            data = request.json
+            if feeders.find(data).count() > 0:
+                print(data, file=sys.stderr)
+                resp = Response(dumps({'bool':True}), status=200, mimetype='application/json')
+            else :
+                resp = Response(dumps({'bool':False}), status=200, mimetype='application/json')
+
+    elif request.method == 'POST':
+        if request.headers['Content-Type'] == 'application/json':
+            data = request.json
+            if data['key'] == "CanIHasCheezeburger":
+                #attempt at hashing to make a key but http requests don't like binary values
+                #sha = hashlib.sha256()
+                print(data, file=sys.stderr)
+
+                ip = request.environ['REMOTE_ADDR']
+                
+                #sha.update(data['id'])
+                #newkey = str(sha.digest())
+                newkey = str(data['id'] * 31)
+                feeder_js = {"id":data['id'], "key":newkey, "ip":ip}
+                
+                if feeders.find({"id":data['id']}).count() > 0:
+                    feeders.update_one({"id":str(data['id'])},{"$set":feeder_js})
+                else:
+                    feeders.insert_one(feeder_js)
+                
+                resp_js_str = dumps({"bool":True, "key":newkey})
+            else: 
+                resp_js_str = dumps({"bool":False, "key":""})
+
+            resp = Response(resp_js_str, status = 200, mimetype = 'application/json')
+
+    return resp
+
 @app.route('/pets/id/<pet_id>', methods = ['GET', 'POST', 'DELETE'])
 def pet_by_id(pet_id):
     db = dbm()
